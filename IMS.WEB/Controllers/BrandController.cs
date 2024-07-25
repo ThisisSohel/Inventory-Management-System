e584ae7db722e9bf441b1e5360d25364fefc65ed
ThisisSohel <offbeat99@yahoo.com>
2024-07-25 14:50:12 +0600

    Complete delete and details action of Brand

--- IMS.WEB/Controllers/BrandController.cs
+++ IMS.WEB/Controllers/BrandController.cs
@@ -41,15 +41,15 @@ namespace IMS.WEB.Controllers
 
             try
             {
-
-                if (ModelState.IsValid)
+                if (brand != null)
                 {
                     await _brandService.CreateBrandService(brand);
                     isValid = true;
+                    message = "Brand is added successfully!";
                 }
                 else
                 {
-                    message = "Model is not bind correctly!";
+                    message = "Something is wrong! Please try again!";
                 }
                 
             }catch (Exception ex)
@@ -207,17 +207,27 @@ namespace IMS.WEB.Controllers
                 Details = brandDetails, 
                 IsSuccess = isSuccess,
                 Message = message,
-            });
+            }, JsonRequestBehavior.AllowGet);
         }
 
 
         [HttpGet]
         public async Task<ActionResult> Update(long id)
         {
+            bool isSuccess = false;
+            string message = string.Empty;
             var updateBrand = new Brand();
             try
             {
                 updateBrand = await _brandService.GetById(id);
+                if (updateBrand == null)
+                {
+                    message = "Brand is not found!";
+                }
+                else
+                {
+                    isSuccess = true;
+                }
 
             }
             catch (Exception ex)
@@ -225,7 +235,12 @@ namespace IMS.WEB.Controllers
                 _logger.Error(ex.Message);
                 ViewBag.Error = ex.Message;
             }
-            return View(updateBrand);
+            return Json(new
+            {
+                UpdateBrandData = updateBrand,
+                IsSuccess = isSuccess,
+                Message = message,
+            }, JsonRequestBehavior.AllowGet);
         }
 
         [HttpPost]
@@ -252,27 +267,36 @@ namespace IMS.WEB.Controllers
             return RedirectToAction("Index");
         }
 
+        [HttpPost]
         public async Task<ActionResult> Delete(long id)
         {
+            string message = string.Empty;
+            bool isSuccess = false;
             var brand = await _brandService.GetById(id);
-            if (brand == null)
-            {
-                return RedirectToAction("Index");
-            }
             try
             {
-                await _brandService.DeleteAsync(id);
-                TempData["AlertMessage"] = "Brand is Deleted successfully!";
-                return RedirectToAction("Index");
+                if(brand.Id != 0) 
+                {
+                    await _brandService.DeleteAsync(id);
+                    message = "Brand is deleted successfully!";
+                    isSuccess = true;
+                }
+                else
+                {
+                    message = "Brand is not found!";
+                }
             }
             catch (Exception ex)
             {
-                _logger.Error(ex.Message);
-                //ViewBag.Error = ex.Message;
-                //TempData["DeleteAlertMessage"] = "Brand is not found!";
+                message = "Internal server error!";
+                //_logger.Error(ex.Message);
             }
-            return View(brand);
 
+            return Json(new
+            {
+                Message = message,
+                IsSuccess = isSuccess
+            }, JsonRequestBehavior.AllowGet);
         }
     }
 }
